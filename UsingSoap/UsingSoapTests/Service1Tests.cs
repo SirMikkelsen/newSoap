c81eb8b6e0a2f20a3cd7e7387b4c731a816ed762
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2018-03-06 14:05:22 +0100

    unit test createt

---
+++ UsingSoap/UsingSoapTests/Service1Tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UsingSoap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingSoap.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetPrisTest()
+        {
+            // Arrange
+            Service1 b1 = new Service1();
+            int expectedResult = 240;
+            // Act
+            var actualResult = b1.GetPris();
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
